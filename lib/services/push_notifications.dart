6a19cb399e579e1295f0c2b124e44cfb2d6696de
Androz2091 <androz2091@gmail.com>
2020-10-20 10:33:38 +0200

    :sparkles: Add push notifications

---
+++ lib/services/push_notifications.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PushNotificationsManager {
+
+  PushNotificationsManager._();
+
+  factory PushNotificationsManager() => _instance;
+
+  static final PushNotificationsManager _instance = PushNotificationsManager._();
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  bool _initialized = false;
+
+  Future<void> init() async {
+    if (!_initialized) {
+      // For iOS request permission first.
+      _firebaseMessaging.requestNotificationPermissions();
+      _firebaseMessaging.configure();
+
+      // For testing purposes print the Firebase Messaging token
+      String token = await _firebaseMessaging.getToken();
+      print("FirebaseMessaging token: $token");
+      await (await SharedPreferences.getInstance()).setString('fcm-token', token);
+
+      _initialized = true;
+    }
+  }
+}
