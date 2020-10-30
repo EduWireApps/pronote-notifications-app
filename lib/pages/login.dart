c50bf7c12624c76f09b1d7882707f07e61d0c2ab
Androz2091 <androz2091@gmail.com>
2020-10-30 18:51:41 +0100

    :bug: Update notification icon

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -165,7 +165,7 @@ class _LoginPageState extends State<LoginPage> {
 				child: CircleAvatar(
 				backgroundColor: Colors.transparent,
 				radius: 48.0,
-				child: Image.asset('assets/pronote-icon.png'),
+				child: Image.asset('assets/launcher-icon.png'),
 				),
 			),
 		);
