f8759c34e46f65efb4d4f820b126e4263bd1f2be
Androz2091 <androz2091@gmail.com>
2020-10-30 15:30:31 +0100

    :bug: Fixes on firebase notifications

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-
 final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+final initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/launcher_icon');
+final initializationSettingsIOS = IOSInitializationSettings();
+final initializationSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
 
 bool _initialized = false;
 
@@ -13,11 +15,13 @@ void initFirebase()
     return;
   }
 
+  _flutterLocalNotificationsPlugin.initialize(initializationSettings);
   _initialized = true;
 
   _firebaseMessaging.configure(
     onMessage: (Map<String, dynamic> message) async {
       print("onMessage: $message");
+      if (message['data']['type'] != null) showNotification(message);
     },
     onLaunch: (Map<String, dynamic> message) async {
       print("onLaunch: $message");
@@ -30,16 +34,12 @@ void initFirebase()
 }
 
 showNotification(Map<String, dynamic> notificationData) async {
-    final android = new AndroidNotificationDetails(
-      notificationData['notification']['title'],
-      notificationData['notification']['title'],
-      notificationData['notification']['title'],
-      styleInformation: BigTextStyleInformation('')
-    );
+    final title = notificationData['notification']['title'];
+    final body = notificationData['notification']['body'];
+    final android = new AndroidNotificationDetails(title, title, title, styleInformation: BigTextStyleInformation(''));
     final iOS = new IOSNotificationDetails();
     final platform = new NotificationDetails(android: android, iOS: iOS);
-    await flutterLocalNotificationsPlugin.show(
-        0, notificationData['notification']['title'], notificationData['notification']['body'], platform);
+    await _flutterLocalNotificationsPlugin.show(0, title, body, platform);
   }
 
 Future<String> getDeviceToken() async {
