1927fa40f929c647d274d9656ce55bd43cab370a
Androz2091 <androz2091@gmail.com>
2021-04-21 14:16:15 +0200

    :sparkles: Open Pronote on click on notif
    Closes #3

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -3,6 +3,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:pronote_notifications/pages/notifications.dart';
+import 'package:external_app_launcher/external_app_launcher.dart';
 
 final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
@@ -22,18 +23,32 @@ void initFirebase(GlobalKey<NavigatorState> navigatorKey) {
 
   Firebase.initializeApp();
 
-  _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  void handleSelectNotification () async {
+    if (await LaunchApp.isAppInstalled(androidPackageName: 'com.IndexEducation.Pronote', iosUrlScheme: 'pronote://')) {
+        print('Launching Pronote...');
+        LaunchApp.openApp(androidPackageName: 'com.IndexEducation.Pronote', iosUrlScheme: 'pronote://');
+      } else {
+        print('Pronote is not installed...');
+        navigatorKey.currentState
+          .push(MaterialPageRoute(builder: (context) => NotificationsPage()));
+      }
+  }
+
+  Future<void> onSelectNotification (String payload) {
+    handleSelectNotification();
+    return null;
+  }
+
+  _flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
   _initialized = true;
 
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
     print("message received from Firebase Messaging");
     if (message.data['type'] != null) showNotification(message);
   });
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
     print("notification clicked by the user");
-    if (message.data['type'] != null)
-      navigatorKey.currentState
-          .push(MaterialPageRoute(builder: (context) => NotificationsPage()));
+    if (message.data['type'] != null) handleSelectNotification();
   });
 }
 
