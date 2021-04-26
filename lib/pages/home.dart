d96fbf883d586e299f2c435aa10c578f44f087aa
Androz2091 <androz2091@gmail.com>
2021-04-26 08:12:44 +0200

    :bug: Specify apple ID for in app review

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
                 if (value == 'À propos') {
                   showAboutAppDialog(context);
                 } else {
-                  inAppReview.openStoreListing();
+                  inAppReview.openStoreListing(appStoreId: '1564109971');
                 }
               },
               itemBuilder: (BuildContext context) {
